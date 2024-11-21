b77612e08b636858b0e61374331e018936681cf7
Juan Camilo <juankamilo@gmail.com>
2024-11-21 17:12:53 -0500

    API Lista RC1

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const path = require('path');
 const app = express();
 app.use(express.static('public'));
 
-const readData = () => {
+const readFile = () => {
 	try {
-		const filePath =  path.join(__dirname, '../public/db.json');
+		const filePath = path.join(__dirname, 'public', 'db.json');
 		const data = fs.readFileSync(filePath);
 		return JSON.parse(data);
 	} catch (error) {
@@ -15,22 +15,19 @@ const readData = () => {
 	}
 }
 
+app.listen(3000, () => {
+	console.log("Escuchando");
+})
 
-// A simple get greet method
 app.get("/oc", (req, res) => {
-    // get the passed query
-    //const { numero } = req.query;
-    //res.send({ oc: `orden ${numero}!` });
-	//const data = readData();
-	//res.json(data);
-	const filePath = path.join(__dirname, '../public/db.json');
-	fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error al leer el archivo:', err);
-            return res.status(500).send('Error al leer el archivo');
-        }
-        res.send(`Contenido del archivo: ${data}`);
-    });
+	const data = readFile();
+	res.json(data);
+});
+
+app.get("/oc/:id", (req, res) => {
+	const data = readFile();
+	const id = req.params.id;
+	res.json(data);
 });
 
 // export the app for vercel serverless functions

caeaee5a2bf059450d73514fc81b292842467376
Juan Camilo <juankamilo@gmail.com>
2024-11-21 15:49:26 -0500

    Prueba3

--- server.js
+++ server.js
@@ -3,22 +3,26 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+const readData = () => {
+	try {
+		const filePath =  path.join(__dirname, '../public/db.json');
+		const data = fs.readFileSync(filePath)
+		return JSON.parse(data);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
 
 // A simple get greet method
 app.get("/oc", (req, res) => {
     // get the passed query
     //const { numero } = req.query;
     //res.send({ oc: `orden ${numero}!` });
-
-	const filePath =  path.join(__dirname, '../public/db.json');
-	fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error al leer el archivo:', err);
-            return res.status(500).send('Error al leer el archivo');
-        }
-        res.send(data);
-    });
+	const data = readData();
+	res.json(data);
 });
 
 // export the app for vercel serverless functions

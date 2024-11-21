180e6f68b598603a1df35612efde342d52386491
Juan Camilo <juankamilo@gmail.com>
2024-11-21 15:25:30 -0500

    Prueba con indicaciones gpt

--- server.js
+++ server.js
@@ -12,8 +12,16 @@ readData();
 // A simple get greet method
 app.get("/oc", (req, res) => {
     // get the passed query
-    const { numero } = req.query;
-    res.send({ oc: `orden ${numero}!` });
+    //const { numero } = req.query;
+    //res.send({ oc: `orden ${numero}!` });
+	const filePath = './db.json';
+	fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error al leer el archivo:', err);
+            return res.status(500).send('Error al leer el archivo');
+        }
+        res.send(`Contenido del archivo: ${data}`);
+    });
 });
 
 // export the app for vercel serverless functions

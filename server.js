ec8361da5b3aeedb9d4f553682aeedd4903cb9e5
Juan Camilo <juankamilo@gmail.com>
2024-12-03 23:42:42 -0500

    Listo con PDF

--- server.js
+++ server.js
@@ -38,18 +38,41 @@ const readFile = () => {
 	}
 }
 
-app.get("/oc", (req, res) => {
-	const id = req.query.id;
-	const data = readFile();
-	data.Encabezado.id = id;
-	res.json(data);
+const readFilePdf = () => {
+	try {
+		const filePath = path.join(__dirname, 'public', 'pdf.json');
+		const data = fs.readFileSync(filePath);
+		return JSON.parse(data);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+app.get("/Values", (req, res) => {
+	const id = req.query.Purchid;
+	console.log(`Parametro id: ${id}`);
+	const pdf = req.query.pdf;
+	console.log(`Parametro pdf: ${pdf}`);
+	const encabezadoData = readFile();
+	const ocFiltrada = encabezadoData.find(item => item.encabezado.ordenCompra === id);
+	console.log(ocFiltrada);
+	const response = ocFiltrada;
+	if (!ocFiltrada) {
+		response = null;
+	} else {
+		if (pdf === "true") {
+			const pdfData = readFilePdf();
+			response.pdf = pdfData;
+		}
+	}
+	res.json(response);
 });
 
-app.get("/oc/:id", (req, res) => {
-	const data = readFile();
-	const id = req.params.id;
-	res.json(data);
-});
+// app.get("/oc/:id", (req, res) => {
+// 	const data = readFile();
+// 	const id = req.params.id;
+// 	res.json(data);
+// });
 
 // export the app for vercel serverless functions
 module.exports = app;
